a90fe7ef4d9b8c9c3cd50d91335217b913a85542
xqli <itxeli@126.com>
2018-07-27 22:17:45 +0800

    add bean

--- src/main/java/com/lxq18/learn/spring/impl/service/TeacherServiceImpl.java
+++ src/main/java/com/lxq18/learn/spring/impl/service/TeacherServiceImpl.java
@@ -1,11 +1,17 @@
 package com.lxq18.learn.spring.impl.service;
 
 import com.lxq18.learn.spring.service.TeacherService;
+import lombok.Data;
+import lombok.Getter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Data
 public class TeacherServiceImpl implements TeacherService {
     private static final Logger logger = LoggerFactory.getLogger(TeacherServiceImpl.class);
+
+    private String name;
+    private int age;
 
     @Override
     public void teach() {
